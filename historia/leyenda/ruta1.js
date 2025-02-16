e8d12a555a4906912c94b9d4e2d138262cd4a5d6
Seeergiio <sergio17727@gmail.com>
2025-02-16 19:53:40 +0100

    Add files via upload

---
+++ historia/leyenda/ruta1.js
@@ -0,0 +1,30 @@
+{
+    "@context": "https://schema.org",
+    "@type": "BreadcrumbList",
+    "itemListElement": [
+      {
+        "@type": "ListItem",
+        "position": 1,
+        "name": "Inicio",
+        "item": "https://seeergiio.github.io/index.html"
+      },
+      {
+        "@type": "ListItem",
+        "position": 2,
+        "name": "Historia",
+        "item": "https://seeergiio.github.io/historia/historia.html"
+      },
+      {
+        "@type": "ListItem",
+        "position": 3,
+        "name": "Leyenda",
+        "item": "https://seeergiio.github.io/historia/leyenda/leyendas.html"
+      },
+      {
+        "@type": "ListItem",
+        "position": 4,
+        "name": "Jugadores",
+        "item": "https://seeergiio.github.io/historia/leyenda/entrenadores.html"
+      }
+    ]
+  }
